f8776af009c3e0482b0df5d6d5bfdbc540fc4715
thiago5171 <Tgazaroli@gmail.com>
2021-04-26 20:16:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,23 @@
 import time
-from funct.allfunc import choose, cadastrar, msg
+from funct.allfunc import choose,  msg, exibir, processando, concluido, transform, abastecer
     
 
 
 #programa principal
-global client
-client  = list()
-file = open("clinetes.txt","rt")
-for linha in file.readlines():
-    if ("\n" == linha) :
-         del(linha)
-    elif ("\n" in linha):
-        del(linha[:-2])
-    else:
-        client.append(linha)
-        print(linha)
-
-file.close()
-a=int((len(client)/3)+1)
-print(client)
-
-
-
 op=0
-while op != 8:
+while op != 5:
     msg()
     op = int(input("Digite o número correspondente a opção da tabela:  "))
-    choose(op)
-    print("\nRealizando operação...\n")
-    #time.sleep(2)
+    choose(op )
+    
 
+
+"""
+preços padroes  
+
+2,66
+3,90
+3,88
+5,99
+
+"""
