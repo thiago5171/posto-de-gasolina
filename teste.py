60e8cb0f8fb8c77a3e4b61fe36688d4f0ca5f84f
thiago5171 <Tgazaroli@gmail.com>
2021-04-26 20:16:48 -0300

    a

--- teste.py
+++ teste.py
@@ -1,3 +1,9 @@
-t=['drdte',333]
-t0=print(t)
-print(t0)
+t=['2.67\n', '3.90\n', '3.88\n', '5.99']
+t2=list()
+aux=0
+for i2, i in enumerate(t):
+    aux=float(i)
+    t2.append(aux)
+    print(i2," ",i)
+t[2] =00000000,3
+print(t2,"\n",t)
